be295f51f1ed2a21a77f92e9dc3d1ad6658e62b6
VLmb <vlamb0809@gmail.com>
2025-07-31 17:20:46 +0300

    Initial commit

---
+++ src/main/java/com/github/VLmb/java_book_trader/JavaBookTraderApplication.java
@@ -0,0 +1,13 @@
+package com.github.VLmb.java_book_trader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBookTraderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBookTraderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/VLmb/java_book_trader/JavaBookTraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.VLmb.java_book_trader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBookTraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
